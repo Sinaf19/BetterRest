b36f2ccd713e76cbe939df73aeb82a87e95e5493
Sinaf19 <quentinsurdez@gmail.com>
2024-01-19 19:06:32 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Quentin Surdez on 19.01.24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
